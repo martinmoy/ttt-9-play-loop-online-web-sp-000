d53947006b3b62dd3c7df400a8fcdbb69c1db3a6
Martin Moy <martinmoy1991@gmail.com>
2019-10-06 16:46:06 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,27 +1,34 @@
-# Helper Methods
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+def input_to_index(input)
+ index = input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
+
+def move(board, index, value = "O" || "X")
+  board[index] = value
+  return board
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " " || board[index] == "")
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
 end
+
 
 def turn(board)
   puts "Please enter 1-9:"
